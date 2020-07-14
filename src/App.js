c28e42804de5ba36a9833d5fbd30952dc4c3278a
Sambalicious <stemitope370@gmail.com>
2020-07-14 17:59:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import React from 'react';
 import {useSelector, useDispatch } from 'react-redux'
 import './App.css';
 import { ADD_NUMBER, SUBSTRACT_NUMBER } from './redux/Types';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import LandingPage from './components/Home/LandingPage';
 
 function App() {
   
-    const dispatch = useDispatch();
-    const number = useSelector(state =>state.number.number);
+    // const dispatch = useDispatch();
+    // const number = useSelector(state =>state.number.number);
     
 
   return (
     <div className="App">
       
-    <div>
+    {/* <div>
        <h2> status - {number}</h2>
         <button onClick={()=>dispatch({type:ADD_NUMBER})}>add number</button>
         <button onClick={()=>dispatch({type:SUBSTRACT_NUMBER})}>sustract number</button>
-    </div>
+    </div> */}
+  
+      <BrowserRouter>
+        <Switch>
+
+           <Route path="/" exact component={LandingPage} />
+        </Switch>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className="">
+            Hello world
+        </div>
+    )
+}
+
+export default LandingPage
